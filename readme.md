6b93cead9f7d32cfcb401e6a170c206ec23eff53
silkwave <silkwave24@gmail.com>
2025-03-29 19:26:39 +0900

    redis-cli

---
+++ readme.md
@@ -0,0 +1,5 @@
+podman exec -it redis bash
+podman exec -it redis redis-cli
+FLUSHALL
+SCAN 0
+GET  1111
--- src/main/java/redis/nyc/redis/RedisApplication.java
+++ src/main/java/redis/nyc/redis/RedisApplication.java
@@ -1,32 +1,19 @@
 package redis.nyc.redis;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
-import redis.nyc.redis.Service.RedisService;
 
 @Slf4j // Lombok을 사용하여 Logger 자동 생성
 @SpringBootApplication
-public class RedisApplication implements CommandLineRunner {
+public class RedisApplication  {
 
-    @Autowired
-    private RedisService redisService;
 
     public static void main(String[] args) {
         SpringApplication.run(RedisApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        // Redis에 데이터 저장
-        redisService.saveString("user:name", "John Doe");
 
-        // Redis에서 데이터 조회
-        String value = redisService.getString("user:name");
-        log.info("Retrieved from Redis: {}", value);
-    }
 
 }
